f0d44d949d8376fa7ed45fafc856608e841ccbf0
AndreasChristensen89 <brogaard89@gmail.com>
2021-10-19 09:53:36 +0000

    Add authorization, need to fix errno111 error

--- project/urls.py
+++ project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reservation.urls'), name='reservation_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
