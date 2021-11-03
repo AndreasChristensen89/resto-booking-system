93a6eb00164ca2e6b79ecfd436cd56ef665cbce8
AndreasChristensen89 <brogaard89@gmail.com>
2021-11-03 12:52:43 +0000

    Add temp. function to auto add end_time in reservations

--- reservations/models.py
+++ reservations/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from datetime import timedelta
 
 
 STATUS = ((0, "Pending"), (1, "Approved"), (2, "Declined"))
@@ -27,16 +28,17 @@ class Reservation(models.Model):
     number_guests = models.PositiveIntegerField()
     date = models.DateField()
     time = models.TimeField()
-    time_end = models.TimeField()
+    time_end = models.TimeField(blank=True, null=True)
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     comment = models.TextField(max_length=300)
 
-    def save(self, *args, **kwargs):
-        if not self.slug and self.first_name and self.last_name:
-            self.slug = slugify(self.first_name+self.last_name)
-        super(Reservation, self).save(*args, **kwargs)
+    def save(self):
+        if not self.time_end and self.time:
+            self.time_end = self.time
+            self.time_end += timedelta(hours=3)
+        super(Reservation, self).save()
 
     class Meta:
         ordering = ["-created_on"]
