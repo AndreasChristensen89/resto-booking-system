ea1f9cd0603f2c0932508448cec4f76cd1288f60
AndreasChristensen89 <brogaard89@gmail.com>
2021-12-05 15:19:24 +0000

    Redeploy, changed allowed host name

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-restaurant-booking.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['booking-system-p4.herokuapp.com', 'localhost']
 
 
 # Application definition
