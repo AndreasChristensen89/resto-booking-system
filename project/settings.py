24f94aa463e578dc0511bfbff9a7efd7171a9248
AndreasChristensen89 <brogaard89@gmail.com>
2021-12-09 13:40:53 +0000

    Add crispyforms to forms

--- project/settings.py
+++ project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
 
     # My apps
     'homepage',
@@ -62,6 +63,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # ACCOUNT_EMAIL_VERIFICATION = 'none'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
