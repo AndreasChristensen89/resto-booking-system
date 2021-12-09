d160867efb181d8227c40d390324bae2eafadd3a
AndreasChristensen89 <brogaard89@gmail.com>
2021-12-09 08:54:51 +0000

    Add summernote to meals description

--- menu/admin.py
+++ menu/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Category, Meals
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Meals)
-class MealsAdmin(admin.ModelAdmin):
+class MealsAdmin(SummernoteModelAdmin):
 
+    summernote_fields = ('description')
     list_display = ('name', 'category')
     search_fields = ['name', 'category', 'for_nbr_people', 'price']
     list_filter = ('category', 'for_nbr_people', 'price')
--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,7 @@ from reservations.views import show_tables
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('reservations/', include('reservations.urls'), name='reservations_urls'),
     path('menu/', include('menu.urls'), name='menu_urls'),
     path('accounts/', include('allauth.urls')),
