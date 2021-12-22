6c38e2e3e5409f83841b0f5941f2a96aeea372cd
AndreasChristensen89 <brogaard89@gmail.com>
2021-12-22 16:40:11 +0000

    Fix django messages styling, shorten time

--- project/settings.py
+++ project/settings.py
@@ -63,6 +63,14 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # ACCOUNT_EMAIL_VERIFICATION = 'none'
