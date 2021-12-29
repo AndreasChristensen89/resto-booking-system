c7bb27d52bc3f5792dc0c900d9900a81813e8b98
AndreasChristensen89 <brogaard89@gmail.com>
2021-12-29 11:42:18 +0000

    Expand menu design, not done, with alternatinv pics+text

--- menu/views.py
+++ menu/views.py
@@ -7,10 +7,14 @@ from .models import Category, Meals
 def menu_list(request):
     menu_list = Meals.objects.all()
     categories = Category.objects.all()
+    list_odd = [3, 7, 11, 15, 19]
+    list_even= [4, 8, 12, 16, 20]
 
     context = {
         'menu_list': menu_list,
         'categories': categories,
+        'list_odd': list_odd,
+        'list_even': list_even,
     }
 
     return render(request, 'menu_list.html', context)
