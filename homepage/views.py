9020ff560d6d52b839b8614d0a72c420f5370041
AndreasChristensen89 <brogaard89@gmail.com>
2022-01-05 18:52:33 +0000

    Fix link to 500.html

--- homepage/views.py
+++ homepage/views.py
@@ -12,4 +12,4 @@ def custom_page_not_found_view(request, exception):
 
 
 def custom_error_view(request, exception=None):
-    return render(request, "errors/500.html", {})
+    return render(request, "500.html", {})
