a29a48777fc2a9735e02cf5d05fa35ab9f98a034
AndreasChristensen89 <brogaard89@gmail.com>
2023-08-29 21:12:05 +0000

    set debug to false

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["localhost", "https://resto-booking-system.onrender.com/", "8000-turquoise-goose-2gjua3vm.ws-eu104.gitpod.io"]
