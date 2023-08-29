b6da4d8b5cfd2a0aa0e4a8b578239e0f59cefc57
AndreasChristensen89 <brogaard89@gmail.com>
2023-08-29 20:40:15 +0000

    setup for new hosting service

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["resto-booking-system.herokuapp.com", "localhost"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
